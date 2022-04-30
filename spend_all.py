bb9d0619f60271f76154468bae6e651cfbd9d83f
Panagiotis Doupidis <72436005+androidDev18026@users.noreply.github.com>
2022-04-30 21:48:36 +0300

    Update spend_all.py

--- spend_all.py
+++ spend_all.py
@@ -1,4 +1,5 @@
 import argparse
+import logging
 
 from bitcoinutils.setup import setup
 from bitcoinutils.utils import to_satoshis
@@ -6,147 +7,189 @@ from bitcoinutils.transactions import Transaction, TxInput, TxOutput
 from bitcoinutils.keys import P2pkhAddress, P2shAddress, PrivateKey, PublicKey
 from bitcoinutils.script import Script
 from bitcoinutils.proxy import NodeProxy
+from enum import Enum
 
-FEE = 2
+MIN_CONF = 0
 
-class InvalidTransactionError(Exception): 
+
+class InvalidTransactionError(Exception):
     __module__ = Exception.__module__
+
+
+class FeeRate(Enum):
+    '''
+    testnet fee rates (as of 30/4/22) from : https://live.blockcypher.com/btc-testnet/ 
+    each value is the BTC amount per KB        
+    '''
+    HIGH = 0.00171   # high priority (1-2 blocks)
+    MEDIUM = 0.00078  # medium priority (3-6 blocks)
+    LOW = 0.00042    # low priority (7+ blocks)
 
 
 def main():
     setup('regtest')
 
-    # proxy.loadwallet('my_wallet')
-    
-    parser = argparse.ArgumentParser(description='Spend all funds from P2SH address to P2PKH address',
-        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=35))
+    logging.basicConfig(format='[%(levelname)s] %(asctime)s : %(message)s',
+                        datefmt='%d/%m/%Y %H:%M:%S', level=logging.INFO)
+    logger = logging.getLogger()
 
-    parser.add_argument('-sk1', type=str, metavar='Private_Key_#1', required=True, help='First private key')
-    parser.add_argument('-sk2', type=str, metavar='Private_Key_#2', required=True, help='Second private key')
-    parser.add_argument('-pk', type=str, metavar='Public_Key', required=True, help='Public key')
-    parser.add_argument('-p2pkh_address', type=str, metavar='P2PKH_Address', required=True, help='Valid P2PKH address to send the funds to')
-    parser.add_argument('-p2sh_address', type=str, metavar='P2SH_Address', required=True, help='Valid P2SH address')
+    parser = argparse.ArgumentParser(description='Spend all funds from a 2-of-3 P2SH address to P2PKH address',
+                                     formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=35))
 
-    parser.add_argument('-rpcuser', type=str, metavar='RPC_username', required=True, help='RPC username as defined in the .conf file')
-    parser.add_argument('-rpcpass', type=str, metavar='RPC_password', required=True, help='RPC password as defined in the .conf file')
+    parser.add_argument('-sk1', type=str, metavar='Private_Key_#1',
+                        required=True, help='First private key')
+    parser.add_argument('-sk2', type=str, metavar='Private_Key_#2',
+                        required=True, help='Second private key')
+    parser.add_argument('-pk', type=str, metavar='Public_Key',
+                        required=True, help='Public key')
+    parser.add_argument('-p2pkh_address', type=str, metavar='P2PKH_Address',
+                        required=True, help='Valid P2PKH address to send the funds to')
+    parser.add_argument('-p2sh_address', type=str, metavar='P2SH_Address',
+                        required=True, help='Valid P2SH address')
+
+    parser.add_argument('-rpcuser', type=str, metavar='RPC_username',
+                        required=True, help='RPC username as defined in the .conf file')
+    parser.add_argument('-rpcpass', type=str, metavar='RPC_password',
+                        required=True, help='RPC password as defined in the .conf file')
 
     args = parser.parse_args()
 
-    proxy = NodeProxy(args.rpcuser, args.rpcpass).get_proxy() # lofos, 1234
+    proxy = NodeProxy(args.rpcuser, args.rpcpass).get_proxy()
 
-    # Accept 2 private keys
-    sk1 = PrivateKey(args.sk1) #('cQq6hbURR3KePybs6h9BjVXa9kLjd7QJiYwm3cLs6QXxaAveLHtA')
-    sk2 = PrivateKey(args.sk2) #('cSK92CY29wDni3V7gT4kQLDqsy1FmRXEEt3kHtzGM5Puf2iSbpxC')
+    # accept 2 private keys
+    sk1 = PrivateKey(args.sk1)
+    sk2 = PrivateKey(args.sk2)
 
-    # Accept 1 public key
-    pk = PublicKey(args.pk) #('02d21464d7339e58526680f8c7c20fd6946f72fd4717c82214b6d9ea9422d08287')
+    # accept 1 public key
+    pk = PublicKey(args.pk)
 
     # recrate the redeem script
     redeem_script = Script(['OP_2', sk1.get_public_key().to_hex(), sk2.get_public_key().to_hex(),
                             pk.to_hex(), 'OP_3', 'OP_CHECKMULTISIG'])
 
     # accept p2sh address
-    p2sh_addr = P2shAddress(args.p2sh_address) #('2MvrsM64GkuhAvFaxbrR25omMwdBko38GyH')
+    p2sh_addr = P2shAddress(args.p2sh_address)
+
+    # check if there is a mismatch between the P2SH address provided and the one we can recreate based on the user's input
+    if p2sh_addr.to_string() != P2shAddress.from_script(redeem_script).to_string():
+        raise ValueError(
+            'P2SH address provided cannot be derived from the public keys provided')
 
     # accept a p2pkh address to send funds to
-    p2pkh_addr = P2pkhAddress(args.p2pkh_address) #('n1MUmxZihh16dcLHD4ZupPEaUVUTj99XoM')
-    
-    p2sh_unspent = proxy.listunspent(0, 0, [p2sh_addr.to_string()])
+    p2pkh_addr = P2pkhAddress(args.p2pkh_address)
 
-    if not p2sh_unspent:
-        raise RuntimeError('There are no UTXO\'s currently associated with this P2SH address')
+    # determine the total amount of blocks in the chain
+    n_blocks = proxy.getblockcount()
 
-    print('=' * 60)
-    utxo = list()
-    for i, tx in enumerate(p2sh_unspent):
-        # print(tx)
-        if tx['amount'] > 0 and tx['safe']:
-            utxo += [tx]
-            print('%d. Found UTXO from P2SH address to spend funds with txid : %s (Amount=%.7f BTC)' %(i+1, tx['txid'], tx['amount']))
-        else:
-            raise RuntimeError('No UTXO\'s associated with that P2SH address')
+    # now we can retrieve the total number of transactions in the blockchain
+    total_txs = sum([proxy.getblockstats(block, ['txs'])['txs']
+                    for block in range(1, n_blocks)])
+    logger.info(
+        'Found a total of {} transactions in the blockchain'.format(total_txs))
 
+    # list all transactions in the blockchain
+    all_txs = proxy.listtransactions('*', total_txs, 0, True)
 
-    # calculate the fees and the amount to send
-    tx_fee, amount_to_send = 0, 0
-    txin = []
-    for tx in utxo:
-        raw_tx = proxy.decoderawtransaction(proxy.getrawtransaction(tx['txid']))
-        tx_fee += raw_tx['size'] * FEE
+    # filter all of the txs above to keep just the ones that involve the P2SH address
+    p2sh_txs = [*filter(lambda tx: tx['address'] ==
+                        p2sh_addr.to_string(), all_txs)]
 
-        txin += [TxInput(tx['txid'], tx['vout'])]
-        amount_to_send += to_satoshis(tx['amount'])
+    logger.info(
+        '{} transactions concerning the given P2SH address'.format(len(p2sh_txs)))
 
-    print("=" * 60)  
+    p2sh_utxo = []
+    amount_to_send = 0.
 
-    print('Total spendable amount aggregated from %d UTXO(s) associated with P2SH address: %.7f BTC' % (len(p2sh_unspent), amount_to_send/10e7))
-    print('TX fee in satoshis: %.1f (%d sats/byte)' % (tx_fee, FEE))
+    '''
+    Iterate over all p2sh_txs and add them as inputs to the tx we'll construct later.
+    Also make sure that these txs are incoming to that address and have at least MIN_CONF confirmations.
+    For the sake of this assignment we'll accept them even if they have 0 confirmations (at least in mempool).
+    '''
+    for tx in p2sh_txs:
+        # sanity check to make sure that the tx is not already spent
+        if not proxy.gettxout(tx['txid'], tx['vout']):
+            logger.warning('tx [{}] already spent'.format(tx['txid']))
+            continue
+        if int(tx['confirmations']) >= MIN_CONF and tx['category'] == 'send':
+            tx_in = TxInput(tx['txid'], tx['vout'])
+            logger.info(
+                '*Found UTXO with {0:.8f} tBTC, TXID : [{1}]'.format(abs(tx['amount']), tx['txid']))
 
+            p2sh_utxo += [tx_in]
+            amount_to_send += abs(float(tx['amount']))
+
+    if not p2sh_utxo:
+        raise RuntimeError(
+            'No UTXO\'s currently associated with that P2SH address')
+
+    # say we want our transaction to have a high priority
+    fee_rate = FeeRate.HIGH
+    PRICE_PER_BYTE = fee_rate.value / 1024
+
+    # calculate fees using this formula : (n_inputs * 148 + n_outputs * 34 + 10) * price_per_byte
+    tx_fee = (len(p2sh_utxo) * 148 + 1 * 34 + 10) * PRICE_PER_BYTE
+
+    logger.info('Total spendable amount aggregated from {0} UTXO(s) associated with P2SH address: {1:.8f} tBTC'.format(
+        len(p2sh_utxo), amount_to_send))
+    logger.info('TX fee in satoshis: {0} (Fee Rate is {1:.8f} tBTC/KB)'.format(
+        to_satoshis(tx_fee), fee_rate.value))
+
+    '''
+    Deduct calculated fees from our initial amount.
+    Difference between (total_inputs - total_outputs) is the fee since all inputs must be spent completely.
+    '''
     amount_to_send -= tx_fee
-    
-    print("=" * 60)
-    print('Sending %ld satoshis (%.10f BTC) to P2PKH address (%s).Transaction fees amount to %d satoshis' 
-                % (amount_to_send, amount_to_send/10e7, p2pkh_addr.to_string(), tx_fee))
-    print("=" * 60)
 
-    txout = TxOutput(amount_to_send, p2pkh_addr.to_script_pub_key())
+    logger.info('Sending {0} satoshis ({1:.8f} BTC) to P2PKH address [{2}]'.format(
+        to_satoshis(amount_to_send), amount_to_send, p2pkh_addr.to_string()))
 
-    tx = Transaction(inputs=txin, outputs=[txout])
+    # construct the tx output by specifying the amount to send and locking them to the P2PKH address provided
+    txout = TxOutput(to_satoshis(amount_to_send),
+                     p2pkh_addr.to_script_pub_key())
+
+    # construct the transaction with N inputs (N >= 1) and 1 output
+    tx = Transaction(inputs=p2sh_utxo, outputs=[txout])
 
     unsigned_raw_tx = tx.serialize()
+    print("=" * 60)
     print('\nRaw unsigned transaction: %s\n' % unsigned_raw_tx)
     print("=" * 60)
 
-    sigs_1 = [sk1.sign_input(tx, i, redeem_script) for i in range(len(txin))]
+    # sign each of the inputs using the 2 private private keys
+    # we need at least 2 of the 3 parties to sign the inputs otherwise the funds cannot be transferred
+    for tx_idx, txin in enumerate(p2sh_utxo):
+        sig1 = sk1.sign_input(tx, tx_idx, redeem_script)
+        sig2 = sk2.sign_input(tx, tx_idx, redeem_script)
 
-    for sig, t in zip(sigs_1, tx.inputs):
-        t.script_sig = Script([sig, redeem_script.to_hex()])
-    
-    part_signed_tx = tx.serialize()
-    
-    # print raw partially signed transaction 
-    print("\nRaw partially signed transactions: %s\n" % part_signed_tx)
-    print("=" * 60)
+        # add OP_0 in unlocking script due to a bug in multisig scripts
+        txin.script_sig = Script(['OP_0', sig1, sig2, redeem_script.to_hex()])
 
-    signed_tx_2 = Transaction.from_raw(part_signed_tx)
+    final_signed_tx = tx.serialize()
 
-    sigs_2 = [sk2.sign_input(signed_tx_2, i, redeem_script) for i in range(len(txin))]
-
-    for sig1, sig2, t in zip(sigs_1, sigs_2, signed_tx_2.inputs):
-        t.script_sig = Script(['OP_0', sig1, sig2, redeem_script.to_hex()])
-
-    final_signed_tx = signed_tx_2.serialize()
-    
-    # print raw signed transaction ready to be broadcasted
-    print("\nFinal raw signed transactions: %s\n" % final_signed_tx)
+    # display raw signed transaction ready to be broadcasted
+    print("\nRaw signed transaction: %s\n" % final_signed_tx)
     print("=" * 71)
 
-    print("Tx Id: %s" % signed_tx_2.get_txid())
+    # display the transaction id
+    print("Tx ID: %s" % tx.get_txid())
     print("=" * 71)
 
     # confirm the validity of the transaction by performing a mempool acceptance test
-    accepted = proxy.testmempoolaccept([final_signed_tx])[0]
+    response_status = proxy.testmempoolaccept([final_signed_tx])[0]
 
-    is_valid = False
-
-    if accepted['allowed']:
-        is_valid = True
-        print()
-        print("=" * 45)
-        print('Transaction is valid, ready to be broadcasted', end='\n')
-        print("=" * 45)
+    if response_status['allowed']:
+        logger.info('Transaction is valid, ready to be broadcasted')
     else:
-        raise InvalidTransactionError('Transaction is not valid therefore it won\'t be broadcasted')
+        raise InvalidTransactionError('Transaction is not valid therefore it won\'t be broadcasted : {}'.format(
+            response_status["reject-reason"]))
 
-    # broadcast tx 
-    if is_valid:
-        print()
-        print("=" * 45)
-        print('Transaction sent to the blockchain', end='\n')
-        print("=" * 45)
-        proxy.sendrawtransaction(final_signed_tx)
+    # broadcast tx once we established its validity
+    tx_sent = proxy.sendrawtransaction(final_signed_tx)
+    if tx_sent:
+        logger.info('Transaction sent to the blockchain')
+    else:
+        logger.error('Transaction couldn\'t be broadcasted')
 
 
 if __name__ == '__main__':
     main()
-    
